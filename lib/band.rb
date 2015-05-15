816062e96be2090ee04db2fedd7c686eefef87e9
Drew <drew@finstrom.us>
2015-05-15 09:15:02 -0700

    capitalize each word in a band#name

--- lib/band.rb
+++ lib/band.rb
@@ -1,3 +1,14 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
+  before_validation(:capitalize_name)
+
+  private
+
+   define_method(:capitalize_name) do
+     words = self.name.split(" ")
+     words.each do |word|
+       word.capitalize!
+     end
+     self.name = words.join(" ")
+   end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,9 +1,15 @@
 require('spec_helper')
 
 describe(Band) do
-  describe('#sample_method') do
-    it('does something') do
+  describe('#name') do
+    it('upcases band names') do
+      band = Band.create(name: 'the uppers')
+      expect(band.name).to(eq('The Uppers'))
+    end
+  end
 
+  describe('#venues') do
+    it('has many-to-many association of venues and bands') do
       expect('').to(eq(''))
     end
   end
