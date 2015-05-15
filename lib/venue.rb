994da934537b8f1e2793fee66d641d35796c9d50
Drew <drew@finstrom.us>
2015-05-15 09:34:05 -0700

    upcase and validate presence and uniqueness of Venue#name

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,20 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
+
+  validates :name, presence: true, uniqueness: true
+
+  before_validation(:capitalize_name)
+
+
+  private
+
+   define_method(:capitalize_name) do
+     unless self.name == nil
+       words = self.name.split(" ")
+       words.each do |word|
+         word.capitalize!
+       end
+       self.name = words.join(" ")
+     end
+   end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,10 +1,21 @@
 require('spec_helper')
 
 describe(Venue) do
-  describe('#') do
-    it('does something') do
+  describe('#name') do
+    it('upcases venue names') do
+      venue = Venue.create(name: 'doug fir')
+      expect(venue.name).to(eq('Doug Fir'))
+    end
 
-      expect('').to(eq(''))
+    it('requires name param to create new') do
+      venue = Venue.create()
+      expect(venue.id).to(eq(nil))
+    end
+
+    it('requires unique venue names') do
+      venue = Venue.create(name: 'wonder ballroom')
+      venue_clone = Venue.create(name: 'wonder ballroom')
+      expect(venue_clone.id).to(eq(nil))
     end
   end
 end
